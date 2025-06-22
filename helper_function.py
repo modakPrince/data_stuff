ff5a327ec54d3976c22ace7f0be68d276260ba0a
Cat <modakprince@proton.me>
2025-06-22 23:56:14 +0530

    third(tensorflow_callback)

--- helper_function.py
+++ helper_function.py
@@ -1,5 +1,7 @@
 import zipfile
 import os
+import tensorflow as tf
+import datetime
 def unzip_data(file_path, extract_to='.'):
     """
     Unzips the specified zip file to the given directory.
@@ -17,6 +19,11 @@ def walk_directory(directory):
     :param directory: Directory to walk through.
     :return: List of file paths.
     """
-    for dirpath,dirnames ,files in os.walk(directory):
-        print(f'There are {len(files)} and {len(dirnames)} directories in {dirpath}')
+    for dirpath, dirnames, files in os.walk(directory):
+        print(f'There are {len(files)} files and {len(dirnames)} directories in {dirpath}')
 
+def create_tensorboard_callback(dir_name, experiment_name):
+  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir)
+  print(f"Saving TensorBoard log files to: {log_dir}")
+  return tensorboard_callback
