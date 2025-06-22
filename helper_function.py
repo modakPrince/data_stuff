7dfdd9995d08588b25b1155e329f59f75fe3f03a
Cat <modakprince@proton.me>
2025-06-23 00:08:05 +0530

    fourth(Add plot_loss_curves function)

--- helper_function.py
+++ helper_function.py
@@ -2,6 +2,7 @@ import zipfile
 import os
 import tensorflow as tf
 import datetime
+import matplotlib.pyplot as plt
 def unzip_data(file_path, extract_to='.'):
     """
     Unzips the specified zip file to the given directory.
@@ -27,3 +28,28 @@ def create_tensorboard_callback(dir_name, experiment_name):
   tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir)
   print(f"Saving TensorBoard log files to: {log_dir}")
   return tensorboard_callback
+
+def plot_loss_curves(history):
+  """
+  Returns seperate loss curves for training and validation metrics.
+  """
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+
+  accuracy = history.history["accuracy"]
+  val_accuracy = history.history["val_accuracy"]
+
+  epochs = range(len(history.history["loss"]))
+
+  plt.plot(epochs, loss, label = "training loss")
+  plt.plot(epochs, val_loss, label = "val_loss")
+  plt.title("loss")
+  plt.xlabel("epochs")
+  plt.legend()
+
+  plt.figure()
+  plt.plot(epochs, accuracy, label = "training_acuracy")
+  plt.plot(epochs, val_accuracy, label = "val_accuracy")
+  plt.title("accuracy")
+  plt.xlabel("epochs")
+  plt.legend()
